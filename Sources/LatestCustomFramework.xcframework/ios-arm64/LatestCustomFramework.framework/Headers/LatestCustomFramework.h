d98e2f900e29776a4c8e2dafb751376e373fd2a8
Vivek <vivek@visio-apps.com>
2024-09-06 17:36:19 +0530

    Version 0.0.2

---
+++ Sources/LatestCustomFramework.xcframework/ios-arm64/LatestCustomFramework.framework/Headers/LatestCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  LatestCustomFramework.h
+//  LatestCustomFramework
+//
+//  Created by Visioapps on 06/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LatestCustomFramework.
+FOUNDATION_EXPORT double LatestCustomFrameworkVersionNumber;
+
+//! Project version string for LatestCustomFramework.
+FOUNDATION_EXPORT const unsigned char LatestCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LatestCustomFramework/PublicHeader.h>
+
+
